21852bfca0b465d3e162ddd8e0d6b7211b8d2a93
Bane <subinrong@outlook.com>
2024-06-28 17:35:05 +0800

    :sparkles: 进入https://juejin.cn/修改title为我爱掘金

--- entrypoints/background.ts
+++ entrypoints/background.ts
@@ -1,3 +1,12 @@
+// background.ts
 export default defineBackground(() => {
-  console.log('Hello background!', { id: browser.runtime.id });
+  const count = storage.defineItem<number>("local:count", {
+    defaultValue: 0,
+  });
+
+  setInterval(async () => {
+    const _count = await count.getValue();
+    console.log(_count);
+    storage.setItem("local:count", _count + 1);
+  }, 1000);
 });
--- entrypoints/content.ts
+++ entrypoints/content.ts
@@ -1,6 +1,24 @@
+import { createApp } from "vue";
+// @ts-ignore
+import LoveJuejin from "@/components/LoveJuejin.vue";
+
 export default defineContentScript({
-  matches: ['*://*.google.com/*'],
-  main() {
-    console.log('Hello content.');
+  matches: ["<all_urls>"],
+  main(ctx) {
+    const ui = createIntegratedUi(ctx, {
+      position: "inline",
+      anchor: "#juejin",
+      onMount: (container) => {
+        const app = createApp(LoveJuejin);
+        app.mount(container);
+        return app;
+      },
+      onRemove: (app) => {
+        if (app) {
+          app.unmount();
+        }
+      },
+    });
+    ui.mount();
   },
 });
